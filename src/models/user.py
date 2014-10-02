2d67efc0e1ab845c3354353a750f055abd654509
root <disgone@gmail.com>
2014-10-02 14:11:27 -0400

    small fixe for (T)token

--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,6 @@
 class User(object):
     def __init__(self, access_token=None):
-        self.token = access_token
+        self.Token = access_token
 
     def is_authenticated(self):
-        return self.token is not None and self.token.is_valid()
+        return self.Token is not None and self.Token.is_valid()
